f5c4d8b6bca3ceba4acda280504e23559ce22b30
Roger López <zroger@users.noreply.github.com>
2019-05-11 18:20:03 -0400

    Add basic log viewing to app.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import LogService from "./LogService.js";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    logs: []
+  };
+
+  /**
+   * When the component mounts, start polling for logs.
+   * The callback appends the new log message to the `logs` array in the state.
+   */
+  componentDidMount() {
+    LogService.poll(log => {
+      this.setState(state => ({
+        logs: state.logs.concat([log])
+      }));
+    });
+  }
+
+  /**
+   * Nothing fancy here. Render logs from the state object.
+   */
+  render() {
+    const { logs } = this.state;
+    return (
+      <div className="App">
+        <div className="logs">
+          {logs.map((log, index) => (
+            <pre key={index}>
+              <code>{log}</code>
+            </pre>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/LogService.js
@@ -0,0 +1,30 @@
+/**
+ * Simulate a log polling API client.
+ */
+const LogService = {
+  /**
+   * This function will invoke the callback with a new log message,
+   * until the logs are exhausted, with a delay between each one.
+   */
+  poll(callback) {
+    fetch("/logs.txt")
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        const logs = data.split("\n").map(line => line.trim());
+        console.debug("Loaded %d lines from log file", logs.length);
+        (function loop(i) {
+          const log = logs[i];
+          if (log) {
+            callback(log);
+            setTimeout(() => {
+              loop(i + 1);
+            }, Math.random() * 500);
+          }
+        })(0);
+      });
+  }
+};
+
+export default LogService;
