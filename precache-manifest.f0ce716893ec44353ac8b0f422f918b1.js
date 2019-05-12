0ab332898d931e8460c56ceef20b24d1ee4216f7
Roger López <zroger@users.noreply.github.com>
2019-05-12 11:26:50 -0400

    Updates

---
+++ precache-manifest.f0ce716893ec44353ac8b0f422f918b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf4da25c6a7660209cdf7518111b74f",
+    "url": "/example-react-logtail/index.html"
+  },
+  {
+    "revision": "d779c1070664b837e6fb",
+    "url": "/example-react-logtail/static/css/main.73e23e2b.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/example-react-logtail/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d779c1070664b837e6fb",
+    "url": "/example-react-logtail/static/js/main.c21c2beb.chunk.js"
+  },
+  {
+    "revision": "d1757a1974be1ff920d8",
+    "url": "/example-react-logtail/static/js/runtime~main.83632b98.js"
+  }
+]);
